293a773467cab4b769710752b87c309db19cb72f
Dillon <ST10079970@vcconnect.edu.za>
2024-05-31 23:25:18 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheFinalFarmer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
